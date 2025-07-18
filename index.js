0d077d62694f2b062bf2cd858edd9a4bae09d2bf
Abhishek <singhbisht84484@gmail.com>
2025-07-18 12:40:05 +0530

    Initialized js file

--- index.js
+++ index.js
@@ -3,3 +3,12 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+let userInput = document.querySelector('.input-text')
+let convertBtn = document.querySelector('.convert-btn')
+let firstpara = document.querySelector('.fsect-para')
+let secondpara = document.querySelector('.ssect-para')
+let thirdpara = document.querySelector('tsect-para')
+
+convertBtn.addEventListener('click', () => {
+  
+})
